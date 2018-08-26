4a00b9074562d4b21a4d22e2472b4cedd0aedaea
Megha Bisht <bishtmegha98@gmail.com>
2018-08-26 20:34:23 +0530

    Create Conditional-statements.cpp

---
+++ Conditional-statements.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+    if(N%2==0)
+    {
+        if(N>=2 && N<=5)
+            cout<<"Not Weird"<<endl;
+        else if(N>=6 && N<=20)
+            cout<<"Weird"<<endl;
+        else if(N>20)
+            cout<<"Not Weird"<<endl;
+    }
+    else
+        cout<<"Weird"<<endl;
+    return 0;
+}
