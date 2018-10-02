f42d75f4a3d3506751f8d901bd1f3156da632de8
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2018-10-02 19:41:08 +0530

    Read comments and instructions

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 using namespace std;
-int main()
-{
+int main() {
     int n;
     cin >> n;
+    int array[n];
+    for ( int i =0; i < n ; i++ ) {
+        cin >> array[i];
+    }
+    /* 
+        I would say that you should write code in some order and it would be better if you take input diractly in array.
+        It would be waste of memory if you use string for taking input. 
+        Apart from that you are just printing in reverse order that's not revesing of array.(that's printing array in
+        reverse form.)
+    */
+    
+    /*
     string arr_temp;
     getline(cin, arr_temp);
     
@@ -14,8 +25,9 @@ int main()
 
         arr[i] = arr_item;
     }
+    */
     for(int i=n-1;i>=0;i--)
-        cout<<arr[i]<<" ";
+        cout<<array[i]<<" ";
     cout<<endl;
 
     return 0;
